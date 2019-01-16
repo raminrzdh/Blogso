3a84635f756dbcabde6dfb523d226162a8208ec2
raminn <raminn@aut.ac.ir>
2019-01-16 17:25:57 +0330

    chl_user

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'chehelso_blog_db');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'chehelso_user');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'myrootpassword');
+define('DB_PASSWORD', 'HKoAuRzr2kfpWnkS');
 
 /** MySQL hostname */
 define('DB_HOST', '172.18.0.2');
