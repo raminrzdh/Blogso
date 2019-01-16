eac521d53dcaf4ed94aa23f67b284a1704c84901
raminn <raminn@aut.ac.ir>
2019-01-16 17:39:42 +0330

    set ip

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'chehelso_user');
 define('DB_PASSWORD', 'HKoAuRzr2kfpWnkS');
 
 /** MySQL hostname */
-define('DB_HOST', '172.18.0.2');
+define('DB_HOST', '172.18.0.2:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
