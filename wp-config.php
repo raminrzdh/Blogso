b19a9a7a2777f7d10a1db1e5916ba2fd6476ef35
raminn <raminn@aut.ac.ir>
2019-01-16 17:11:39 +0330

    localhost

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'chehelso_user');
 define('DB_PASSWORD', 'HKoAuRzr2kfpWnkS');
 
 /** MySQL hostname */
-define('DB_HOST', 'db');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
