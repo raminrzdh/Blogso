62710a63f95579a5bd56489ec8241a7b4c0a20dc
raminn <raminn@aut.ac.ir>
2019-01-16 18:38:33 +0330

    replace wp

--- readme.md
+++
@@ -1,1 +0,0 @@
-#for chehelso blog ;)
--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'chehelso_blog_db');
+define('DB_NAME', 'database_name_here');
 
 /** MySQL database username */
-define('DB_USER', 'blog_user');
+define('DB_USER', 'username_here');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'HKoAuRzr2kfpWnkS');
+define('DB_PASSWORD', 'password_here');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
